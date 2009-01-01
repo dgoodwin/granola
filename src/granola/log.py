dbea77f64a671a46c1a4156e37fb17b72092b9a0
Devan Goodwin <dgoodwin@dangerouslyinc.com>
2009-01-01 16:13:25 -0400

    Rename project "granola" and create sqlite3 db on first run.

--- src/granola/log.py
+++ src/granola/log.py
@@ -1,3 +1,8 @@
+#   Granola - GPS Enabled Open Source Workout/Adventure Log
+#
+#   Copyright (C) 2008 Devan Goodwin <dgoodwin@dangerouslyinc.com>
+#
+#
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; either version 2 of the License, or
